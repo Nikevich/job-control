34de81ce86741f3c64e148c02361f176888b10fb
Nikevich <nikshkilevich@yandex.ru>
2025-10-08 14:42:29 +0300

    Подвязка логера

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from logger_config import setup_common_logger, setup_fastapi_loggers
 
-app = FastAPI()
+# Настройка логирования
+setup_fastapi_loggers()
+logger = setup_common_logger()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("START")
+    yield
+    logger.info("END")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
-    return {"message": "Hellow World"}
+    return {"message": "Hello World"}

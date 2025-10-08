326310dde0352fbd23daae0429ec8fd3f356cb7d
Nikevich <nikshkilevich@yandex.ru>
2025-10-08 15:44:21 +0300

    Тестовый небольшой файл с данными

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from contextlib import asynccontextmanager
 from logger_config import setup_common_logger, setup_fastapi_loggers
 
@@ -17,3 +18,8 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/test-short/")
+async def test_short():
+    file_path = "tmp/data/test-short.json"
+    return FileResponse(path=file_path, media_type="application/jdon", filename="test-short.json")
